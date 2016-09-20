08fd309b2318adbb4c362d194bf595ca208beacd
Steffen Reith <Steffen.Reith@hs-rm.de>
2016-09-20 15:07:45 +0200

    Added the first source

---
+++ src/main/scala/J1Sc.scala
@@ -0,0 +1,15 @@
+/*
+ * Author: <AUTHORNAME> (<AUTHOREMAIL>)
+ * Committer: <COMMITTERNAME>
+ *
+ * Create Date:    Tue Sep 20 15:07:10 CEST 2016 
+ * Module Name:    XXXX - 
+ * Project Name:   XXXX - YYYY
+ *
+ * Hash: <COMMITHASH>
+ * Date: <AUTHORDATE>
+ */
+import spinal.core._
+import spinal.lib._
+
+
