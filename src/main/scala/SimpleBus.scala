40722bd066ae0ae09f2455b6ab23a0054ece2f69
Steffen Reith <steffen.reith@hs-rm.de>
2016-11-13 00:27:59 +0100

    Added a lot of comments. Minor refactoring

--- src/main/scala/SimpleBus.scala
+++ src/main/scala/SimpleBus.scala
@@ -6,6 +6,8 @@
  * Module Name:    SimpleBus - A simple bus used to communicate with GPIO peripherial components
  * Project Name:   J1Sc - A simple J1 implementation in Scala using Spinal HDL
  *
+ * Remark: Initial code provided by: Charles Papon (charles.papon.90@gmail.com)
+ *
  * Hash: <COMMITHASH>
  * Date: <AUTHORDATE>
  */
@@ -15,67 +17,92 @@ import spinal.lib.bus.misc._
 
 case class SimpleBus(addressWidth : Int, dataWidth : Int) extends Bundle with IMasterSlave {
 
-  val enable      = Bool //Bus should be used when 'enable' is high
-  val writeToBus  = Bool //High to write, low to read
-  val address     = UInt(addressWidth bits) //Address (byte-aligned)
-  val writeData   = Bits(dataWidth bits)
-  val readData    = Bits(dataWidth bits)
+  val enable     = Bool // Bus can be used when 'enable' is high
+  val writeMode  = Bool // High to write data, low to read data
+  val address    = UInt(addressWidth bits) // Address (byte-aligned)
+  val writeData  = Bits(dataWidth bits)
+  val readData   = Bits(dataWidth bits)
 
+  // This is called by 'apply' when the master-object is called with data (-> side effect write/read data)
   override def asMaster() : Unit = {
-    out(enable, writeToBus, address, writeData)
+
+    // Write data to the bus
+    out(enable, writeMode, address, writeData)
+
+    // Read data from the bus
     in(readData)
+
   }
+
 }
 
 case class SimpleBusSlaveFactory(bus : SimpleBus) extends BusSlaveFactoryDelayed {
 
-  val readDataReg = Reg(Bits(bus.dataWidth bits)) //Give one cycle delay to reads (is that what you want ?)
+  val readDataReg = Reg(Bits(bus.dataWidth bits)) //Give one cycle delay to reads
   bus.readData := readDataReg
 
-  override def build(): Unit = {
+  // Build the bus
+  override def build() : Unit = {
 
+    // The bus consists out of different BusSlaveFactoryElement's (iterate over the internal list of them)
     for(element <- elements) element match {
 
+      // Only non-stop write mode is implement
       case element : BusSlaveFactoryNonStopWrite =>
+
+        // Write payload of 'getBitsWidth' bits at 'bitOffset' to the BusSlaveFactoryElement
         element.that.assignFromBits(bus.writeData(element.bitOffset, element.that.getBitsWidth bits))
 
+      // Ignore all other write modes
       case _ =>
 
     }
 
+    // There is a list of I/O jobs to be done, hence iterate over them
     for((address, jobs) <- elementsPerAddress) {
 
-      when(bus.address === address){
+      // Check whether the address matches and the bus is enabled
+      when ((bus.address === address) && (bus.enable)) {
 
-        when(bus.enable) {
+        // Check for write - mode
+        when(bus.writeMode) {
 
-          when(bus.writeToBus) {
+          // For all jobs regarding the given address (write mode)
+          for (element <- jobs) element match {
 
-            for (element <- jobs) element match {
+            // Check of  write operation
+            case element : BusSlaveFactoryWrite => {
 
-              case element: BusSlaveFactoryWrite => {
+                // Write payload of 'getBitsWidth' bits at 'bitOffset' to the BusSlaveFactoryElement
                 element.that.assignFromBits(bus.writeData(element.bitOffset, element.that.getBitsWidth bits))
-              }
-
-              case element: BusSlaveFactoryOnWrite => element.doThat()
-
-              case _ =>
 
             }
 
-          } otherwise {
+            // Execute the action which is registered to a write job on the actual address
+            case element : BusSlaveFactoryOnWrite => element.doThat()
 
-            for (element <- jobs) element match {
+            // Ignore all other types of BusSlaveFactoryElements
+            case _ =>
 
-              case element: BusSlaveFactoryRead => {
+          }
+
+        } otherwise {
+
+          // For all jobs regarding the given address (read mode)
+          for (element <- jobs) element match {
+
+            // Check of a Read job
+            case element : BusSlaveFactoryRead => {
+
+                // Read data to the delay - register
                 readDataReg(element.bitOffset, element.that.getBitsWidth bits) := element.that.asBits
-              }
-
-              case element: BusSlaveFactoryOnRead => element.doThat()
-
-              case _ =>
-
             }
+
+            // Execute the action which is registered to a read operation on the actual address
+            case element : BusSlaveFactoryOnRead => element.doThat()
+
+            // Ignore all other types of BusSlaveFactoryElements
+            case _ =>
 
           }
 
@@ -87,6 +114,7 @@ case class SimpleBusSlaveFactory(bus : SimpleBus) extends BusSlaveFactoryDelayed
 
   }
 
+  // Tell the width of data bus
   override def busDataWidth: Int = bus.dataWidth
 
 }
