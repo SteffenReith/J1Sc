4634dcc74fd593a7cd24c90271d715a35ffe3e8c
Steffen Reith <steffen.reith@hs-rm.de>
2016-11-20 00:22:53 +0100

    Reorganization for better dealing with bootcode

--- src/main/scala/J1.scala
+++ src/main/scala/J1.scala
@@ -28,71 +28,21 @@ class J1(cfg : J1Config) extends Component {
   val memWrite       = Bits(cfg.wordSize bits)
   val memRead        = Bits(cfg.wordSize bits)
 
-  // Create main memory
-  def content = List(B"1000_0000_0000_0111", //  0. Push 7
-                     B"1000_0000_0000_0011", //  1. Push 3
-                     B"0000_0000_0000_0100", //  2. Jump 4
-                     B"1000_0000_0000_0001", //  3. Push 1
-                     B"1000_0000_0000_1111", //  4. Push 15
-                     B"0110_0010_0000_0011", //  5. Add, drop and push
-                     B"0110_0010_0000_0001", //  6. Add and push
-                     B"0110_0010_0000_0000", //  7. Add and no push
-                     B"0010_0000_0000_1011", //  8. Jump 11 if tos is zero
-                     B"1000_0000_0000_0000", //  9. Push 0
-                     B"0010_0000_0000_1010", // 10. Jump 10 if tos is zero
-                     B"0100_0000_0010_0000", // 11. Call 32
-                     B"1000_0000_0000_0001", // 12. Push 1
-                     B"0110_0111_0000_0001", // 13. Compare tos and nos push result
-                     B"1000_0000_0000_0011", // 14. Push 3
-                     B"1000_0000_0000_0011", // 15. Push 3
-                     B"0110_0111_0000_0001", // 16. Compare tos and nos push result
-                     B"1101_0101_0101_0111", // 17. Push 0x5557
-                     B"1000_0000_0000_0000", // 18. Push 0x0000
-                     B"0110_0000_0100_0000", // 19. ALU I/O operation
-                     B"0110_0000_0000_0000", // 20. NOP (wait state for I/O)
-                     B"0110_0000_0000_0000", // 21. Clear I/O
-                     B"0110_0000_0000_0000", // 22. Clear I/O (wait state)
-                     B"0000_0000_0001_0111", // 23. Jump 23
-                     B"0110_0000_0000_0000", // 24. NOP
-                     B"0110_0000_0000_0000", // 25. NOP
-                     B"0110_0000_0000_0000", // 26. NOP
-                     B"0110_0000_0000_0000", // 27. NOP
-                     B"0110_0000_0000_0000", // 28. NOP
-                     B"0110_0000_0000_0000", // 29. NOP
-                     B"0110_0000_0000_0000", // 30. NOP
-                     B"0110_0000_0000_0000", // 31. NOP
-                     B"1000_0000_0001_0001", // 32. Push 17
-                     B"1000_0000_0001_0000", // 33. Push 16
-                     B"0110_1111_0000_0001", // 34. Compare unsigned and push
-                     B"1011_1111_1111_1111", // 35. Push +maxint
-                     B"1111_1111_1111_1111", // 36. Push -1
-                     B"0110_1000_0000_0001", // 37. Compare signed and push
-                     B"0110_0001_0000_0011", // 38. Pop
-                     B"0110_0001_0000_0011", // 39. Pop
-                     B"0110_0001_0000_0011", // 40. Pop
-                     B"0110_0001_0000_0011", // 41. Pop
-                     B"0110_0001_0000_0011", // 42. Pop
-                     B"0110_0001_0000_0011", // 43. Pop
-                     B"1000_0000_1111_1110", // 44. Push 254
-                     B"1000_0001_0000_0000", // 45. Push 256
-                     B"0110_0000_0011_0000", // 46. Write to external RAM
-                     B"0110_0001_0000_0011", // 47. Pop
-                     B"0110_0001_0000_0011", // 48. Pop
-                     B"0111_0000_0000_1100") // 49. Return from Subroutine
-  val mainMemory = Mem(Bits(cfg.wordSize bits),
-                            content ++ List.fill((1 << cfg.addrWidth) - content.length)(B(0, cfg.wordSize bits)))
+  // Main memory prefilled with boot code
+  val mainMem = Mem(Bits(cfg.wordSize bits),
+                    cfg.bootCode() ++ List.fill((1 << cfg.addrWidth) - cfg.bootCode().length)(B(0, cfg.wordSize bits)))
 
   // Create a new CPU core
   val coreJ1CPU = new J1Core(cfg)
 
   // Create data port for mainMem
-  mainMemory.write(enable  = memWriteEnable,
-                   address = memAdr,
-                   data    = memWrite)
-  memRead := mainMemory.readSync(address = memAdr, readUnderWrite = readFirst)
+  mainMem.write(enable  = memWriteEnable,
+                address = memAdr,
+                data    = memWrite)
+  memRead := mainMem.readSync(address = memAdr, readUnderWrite = readFirst)
 
   // Instruction port (read only)
-  coreJ1CPU.io.instr := mainMemory.readSync(address = coreJ1CPU.io.instrAdr, readUnderWrite = readFirst)
+  coreJ1CPU.io.instr := mainMem.readSync(address = coreJ1CPU.io.instrAdr, readUnderWrite = readFirst)
 
   // connect the CPU core with the internal memory
   memWriteEnable <> coreJ1CPU.io.memWriteMode
--- src/main/scala/J1Config.scala
+++ src/main/scala/J1Config.scala
@@ -10,15 +10,73 @@
  * Date: <AUTHORDATE>
  */
 
+import spinal.core._
+
 // The configuration of a J1-CPU
 case class J1Config (wordSize : Int,
                      dataStackIdxWidth : Int,
                      returnStackIdxWidth : Int,
                      addrWidth : Int,
-                     startAddress : Int)
+                     startAddress : Int,
+                     bootCode : () => List[Bits])
 
 // Holds the configuration parameters of a J1
 object J1Config {
+
+  // Simply halt the CPU (endless loop of instruction at 0)
+  def endlessLoop() = List(B"0000_0000_0000_0000") // 0. Jump 0
+
+  // Simple test of the ISA
+  def isaTest() = List(B"1000_0000_0000_0111", //  0. Push 7
+                       B"1000_0000_0000_0011", //  1. Push 3
+                       B"0000_0000_0000_0100", //  2. Jump 4
+                       B"1000_0000_0000_0001", //  3. Push 1
+                       B"1000_0000_0000_1111", //  4. Push 15
+                       B"0110_0010_0000_0011", //  5. Add, drop and push
+                       B"0110_0010_0000_0001", //  6. Add and push
+                       B"0110_0010_0000_0000", //  7. Add and no push
+                       B"0010_0000_0000_1011", //  8. Jump 11 if tos is zero
+                       B"1000_0000_0000_0000", //  9. Push 0
+                       B"0010_0000_0000_1010", // 10. Jump 10 if tos is zero
+                       B"0100_0000_0010_0000", // 11. Call 32
+                       B"1000_0000_0000_0001", // 12. Push 1
+                       B"0110_0111_0000_0001", // 13. Compare tos and nos push result
+                       B"1000_0000_0000_0011", // 14. Push 3
+                       B"1000_0000_0000_0011", // 15. Push 3
+                       B"0110_0111_0000_0001", // 16. Compare tos and nos push result
+                       B"1101_0101_0101_0111", // 17. Push 0x5557
+                       B"1000_0000_0000_0000", // 18. Push 0x0000
+                       B"0110_0000_0100_0000", // 19. ALU I/O operation
+                       B"0110_0000_0000_0000", // 20. NOP (wait state for I/O)
+                       B"0110_0000_0000_0000", // 21. Clear I/O               
+                       B"0110_0000_0000_0000", // 22. Clear I/O (wait state)  
+                       B"0000_0000_0001_0111", // 23. Jump 23                 
+                       B"0110_0000_0000_0000", // 24. NOP                     
+                       B"0110_0000_0000_0000", // 25. NOP                     
+                       B"0110_0000_0000_0000", // 26. NOP                     
+                       B"0110_0000_0000_0000", // 27. NOP                     
+                       B"0110_0000_0000_0000", // 28. NOP                     
+                       B"0110_0000_0000_0000", // 29. NOP                     
+                       B"0110_0000_0000_0000", // 30. NOP                     
+                       B"0110_0000_0000_0000", // 31. NOP                     
+                       B"1000_0000_0001_0001", // 32. Push 17                 
+                       B"1000_0000_0001_0000", // 33. Push 16                 
+                       B"0110_1111_0000_0001", // 34. Compare unsigned and push
+                       B"1011_1111_1111_1111", // 35. Push +maxint            
+                       B"1111_1111_1111_1111", // 36. Push -1                 
+                       B"0110_1000_0000_0001", // 37. Compare signed and push 
+                       B"0110_0001_0000_0011", // 38. Pop                     
+                       B"0110_0001_0000_0011", // 39. Pop                     
+                       B"0110_0001_0000_0011", // 40. Pop                     
+                       B"0110_0001_0000_0011", // 41. Pop                     
+                       B"0110_0001_0000_0011", // 42. Pop                     
+                       B"0110_0001_0000_0011", // 43. Pop                     
+                       B"1000_0000_1111_1110", // 44. Push 254                
+                       B"1000_0001_0000_0000", // 45. Push 256                
+                       B"0110_0000_0011_0000", // 46. Write to external RAM   
+                       B"0110_0001_0000_0011", // 47. Pop                     
+                       B"0110_0001_0000_0011", // 48. Pop                     
+                       B"0111_0000_0000_1100") // 49. Return from Subroutine)
 
   // Provide a default configuration
   def default = {
@@ -28,7 +86,8 @@ object J1Config {
                           dataStackIdxWidth   =  8,
                           returnStackIdxWidth =  6,
                           addrWidth           = 13,
-                          startAddress        =  0 )
+                          startAddress        =  0,
+                          bootCode            =  endlessLoop)
 
     // Return the default configuration
     config
@@ -43,7 +102,8 @@ object J1Config {
                           dataStackIdxWidth   =  4,
                           returnStackIdxWidth =  3,
                           addrWidth           =  8,
-                          startAddress        =  0 )
+                          startAddress        =  0,
+                          bootCode            =  isaTest)
 
     // Return the default configuration
     config
