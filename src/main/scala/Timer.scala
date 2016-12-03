7a662efa104c3a43a5a91c1c2d2c1b2f11a0e5e1
Steffen Reith <steffen.reith@hs-rm.de>
2016-12-03 17:34:38 +0100

    Inital version

---
+++ src/main/scala/Timer.scala
@@ -0,0 +1,15 @@
+/*
+ * Author: <AUTHORNAME> (<AUTHOREMAIL>)
+ * Committer: <COMMITTERNAME>
+ *
+ * Creation Date:  Sat Dec 3 17:33:06 GMT+1 2016
+ * Module Name:    Timer - A programmable timer which generates interrupts 
+ * Project Name:   J1Sc - A simple J1 implementation in Scala using Spinal HDL
+ *
+ * Hash: <COMMITHASH>
+ * Date: <AUTHORDATE>
+ */
+import spinal.core._
+import spinal.lib._
+
+
