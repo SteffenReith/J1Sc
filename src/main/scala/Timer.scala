d524298ed5c26f0fa8bace84d91205f182144b56
Steffen Reith <steffen.reith@hs-rm.de>
2016-12-10 07:42:53 +0100

    Fixed the bus interface of the enable register

--- src/main/scala/Timer.scala
+++ src/main/scala/Timer.scala
@@ -20,11 +20,17 @@ class Timer(cfg : TimerConfig) extends Component {
 
   val io = new Bundle {
 
-    val loadHigh  = in Bool
-    val loadLow   = in Bool
-    val valueHigh = in UInt (cfg.width / 2 bits)
-    val valueLow  = in UInt (cfg.width / 2 bits)
-    val enable   = in Bool
+    val loadHigh = in Bool
+    val loadLow  = in Bool
+    val cmpHigh  = in UInt(cfg.width / 2 bits)
+    val cmpLow   = in UInt(cfg.width / 2 bits)
+
+    val enable = in Bits(cfg.width / 2 bits)
+
+    val highState = out UInt(cfg.width / 2 bits)
+    val lowState  = out UInt(cfg.width / 2 bits)
+
+    val enableState = out Bits(cfg.width / 2  bits)
 
     val interrupt = out Bool
 
@@ -35,19 +41,20 @@ class Timer(cfg : TimerConfig) extends Component {
 
   // Compare register (maximal values to count to)
   val cmp = Reg(UInt(cfg.width bits)) init (0)
+  io.highState := cmp(cmp.high downto cfg.width / 2)
+  io.lowState := cmp(cfg.width / 2 - 1 downto 0)
 
-  // Holds the enable flag
-  val enabled = Reg(Bool) init(False)
-
-  // Register the enable signal
-  when(io.enable) {enabled := True}
+  // Register the enable flag (adapt the width for the 1 bit register)
+  val isEnabled = Reg(Bool) init (False)
+  isEnabled := io.enable.orR
+  io.enableState := (default -> isEnabled)
 
   // Check if the low word of the compare register has to be loaded
   when(io.loadLow) {
 
     // Load the compare value, reset the timer and disable it
-    cmp(cfg.width / 2 - 1 downto 0) := io.valueLow
-    enabled := False
+    cmp(cfg.width / 2 - 1 downto 0) := io.cmpLow
+    isEnabled := False
     cnt := 0
 
   }
@@ -56,14 +63,14 @@ class Timer(cfg : TimerConfig) extends Component {
   when(io.loadHigh) {
 
     // Load the compare value, reset the timer and disable it
-    cmp(cmp.high downto cfg.width / 2) := io.valueHigh
-    enabled := False
+    cmp(cmp.high downto cfg.width / 2) := io.cmpHigh
+    isEnabled := False
     cnt := 0
 
   }
 
   // Check if the timer is enabled
-  when(enabled && !(io.loadHigh || io.loadLow)) {
+  when(isEnabled && !(io.loadHigh || io.loadLow)) {
 
     // Count modulo
     when(cnt < cmp - 1) {
@@ -80,28 +87,27 @@ class Timer(cfg : TimerConfig) extends Component {
 
   }
 
-  enabled := True;
-
   // Generate the interrupt signal
-  io.interrupt := enabled && (cnt === 0) && !(io.loadHigh || io.loadLow)
+  io.interrupt := isEnabled && (cnt === 0) && !(io.loadHigh || io.loadLow)
 
   // Implement the bus interface
   def driveFrom(busCtrl : BusSlaveFactory, baseAddress : BigInt) = new Area {
 
     // The lower part of the compare register is mapped at address 0 and is of type r/w
-    busCtrl.read(io.valueLow, baseAddress + 0, 0)
-    busCtrl.nonStopWrite(io.valueLow, 0) // value will be constantly driven by the data of the memory bus
+    busCtrl.read(io.lowState, baseAddress + 0, 0)
+    busCtrl.nonStopWrite(io.cmpLow, 0) // value will be constantly driven by the data of the memory bus
 
     // The higher part of the compare register is mapped at address 0 and is of type r/w
-    busCtrl.read(io.valueHigh, baseAddress + 1, 0)
-    busCtrl.nonStopWrite(io.valueHigh, 0) // value will be constantly driven by the data of the memory bus
+    busCtrl.read(io.highState, baseAddress + 1, 0)
+    busCtrl.nonStopWrite(io.cmpHigh, 0) // value will be constantly driven by the data of the memory bus
 
     // Generate the write enable signals for loading the compare value
     io.loadLow  := busCtrl.isWriting(baseAddress + 0)
     io.loadHigh := busCtrl.isWriting(baseAddress + 1)
 
-    // Enable the time by a simple write access
-    io.enable := busCtrl.isWriting(baseAddress + 2)
+    // A r/w register for enabling the timer (anything != 0 means true)
+    busCtrl.read(io.enableState, baseAddress + 2, 0)
+    busCtrl.nonStopWrite(io.enable, 0) // the enable signal is constantly driven by the data of the memory bus
 
   }
 
