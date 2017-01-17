4b3203a9b044f7920b90d8d71dd9fc0ed9bef7a8
Steffen Reith <steffen.reith@hs-rm.de>
2017-01-17 23:06:08 +0100

    Added different clocking areas

--- src/main/scala/J1SoC.scala
+++ src/main/scala/J1SoC.scala
@@ -12,6 +12,20 @@
 import spinal.core._
 import spinal.lib.com.uart._
 
+// Provide a MMCM
+class MMCM extends BlackBox {
+
+  val io = new Bundle {
+
+    val providedClk = in Bool
+    val synthClk = out Bool
+
+  }
+
+  noIoPrefix()
+
+}
+
 class J1SoC (j1Cfg   : J1Config,
              gpioCfg : GPIOConfig) extends Component {
 
@@ -29,65 +43,93 @@ class J1SoC (j1Cfg   : J1Config,
 
   }.setName("")
 
-  // Create a new CPU core
-  val cpu = new J1(j1Cfg)
+  // Physical clock area
+  val clkCtrl = new Area {
 
-  // Create a delayed version of the cpu core interface to GPIO
-  val peripheralBus = cpu.io.cpuBus.delayed(gpioCfg.gpioWaitStates)
-  val peripheralBusCtrl = SimpleBusSlaveFactory(peripheralBus)
+    // Create a MMCM instance and connect it to the current clock
+    val mmcm = new MMCM
+    mmcm.io.providedClk := ClockDomain.current.readClockWire
 
-  // Create a LED array at base address 0x40
-  val ledArray = new LEDArray(gpioCfg.ledBankConfig)
-  val ledBridge = ledArray.driveFrom(peripheralBusCtrl, 0x40)
+    // Create clock area which is related to the clock synthesized by the MMCM
+    val coreClockDomain = ClockDomain(
 
-  // Connect the physical LED pins to the outside world
-  io.leds := ledArray.io.leds
+      // Use the synthesized clock for this domain
+      clock = mmcm.io.synthClk,
 
-  // Create two timer and map it at 0xC0 and 0xD0
-  val timerA       = new Timer(gpioCfg.timerConfig)
-  val timerABridge = timerA.driveFrom(peripheralBusCtrl, 0xC0)
-  val timerB       = new Timer(gpioCfg.timerConfig)
-  val timerBBridge = timerB.driveFrom(peripheralBusCtrl, 0xD0)
+      // Use the current reset
+      reset = ClockDomain.current.reset,
 
-  // Create an UART interface with fixed capabilities
-  val uartCtrlGenerics = UartCtrlGenerics(dataWidthMax = gpioCfg.uartConfig.dataWidthMax,
-                                          clockDividerWidth = gpioCfg.uartConfig.clockDividerWidth,
-                                          preSamplingSize = gpioCfg.uartConfig.preSamplingSize,
-                                          samplingSize = gpioCfg.uartConfig.samplingSize,
-                                          postSamplingSize = gpioCfg.uartConfig.postSamplingSize)
-  val uartCtrlInitConfig = UartCtrlInitConfig(baudrate = gpioCfg.uartConfig.baudrate,
-                                              dataLength = gpioCfg.uartConfig.dataLength,
-                                              parity = gpioCfg.uartConfig.parity,
-                                              stop = gpioCfg.uartConfig.stop)
-  val uartCtrlMemoryMappedConfig = UartCtrlMemoryMappedConfig(uartCtrlConfig = uartCtrlGenerics,
-                                                              initConfig = uartCtrlInitConfig,
-                                                              busCanWriteClockDividerConfig = false,
-                                                              busCanWriteFrameConfig = false,
-                                                              txFifoDepth = gpioCfg.uartConfig.fifoDepth,
-                                                              rxFifoDepth = gpioCfg.uartConfig.fifoDepth)
-  val uartCtrl = new UartCtrl(uartCtrlGenerics)
+      // Scale the frequency according to the frequency provided by the MMCM
+      frequency = FixedFrequency(ClockDomain.current.frequency.getValue*4/5)
 
-  // Map the UART to 0x80 and enable the generation of read interrupts
-  val uartBridge = uartCtrl.driveFrom16(peripheralBusCtrl, uartCtrlMemoryMappedConfig, baseAddress = 0x80)
-  uartBridge.interruptCtrl.readIntEnable := True
+    )
 
-  // Tell Spinal that some unneeded signals are allowed to be pruned to avoid warnings
-  uartBridge.interruptCtrl.interrupt.allowPruning()
-  uartBridge.write.streamUnbuffered.ready.allowPruning()
+  }
 
-  // Create an interrupt controller, map it to 0xE0 and connect all interrupts
-  val intCtrl = new InterruptCtrl(j1Cfg)
-  val intCtrlBridge = intCtrl.driveFrom(peripheralBusCtrl, 0xE0)
-  intCtrl.io.intsE(intCtrl.io.intsE.high downto j1Cfg.irqConfig.numOfInternalInterrupts) <> io.extInt
-  intCtrl.io.intsE(0) <> uartBridge.interruptCtrl.readInt
-  intCtrl.io.intsE(1) <> timerA.io.interrupt
-  intCtrl.io.intsE(2) <> timerB.io.interrupt
-  cpu.io.intNo <> intCtrl.io.intNo
-  cpu.io.irq <> intCtrl.io.irq
+  // Generate the application specific clocking area
+  val coreArea = new ClockingArea(clkCtrl.coreClockDomain) {
 
-  // Connect the physical UART pins to the outside world
-  io.tx := uartCtrl.io.uart.txd
-  uartCtrl.io.uart.rxd := io.rx
+    // Create a new CPU core
+    val cpu = new J1(j1Cfg)
+
+    // Create a delayed version of the cpu core interface to GPIO
+    val peripheralBus = cpu.io.cpuBus.delayed(gpioCfg.gpioWaitStates)
+    val peripheralBusCtrl = SimpleBusSlaveFactory(peripheralBus)
+
+    // Create a LED array at base address 0x40
+    val ledArray = new LEDArray(gpioCfg.ledBankConfig)
+    val ledBridge = ledArray.driveFrom(peripheralBusCtrl, 0x40)
+
+    // Connect the physical LED pins to the outside world
+    io.leds := ledArray.io.leds
+
+    // Create two timer and map it at 0xC0 and 0xD0
+    val timerA = new Timer(gpioCfg.timerConfig)
+    val timerABridge = timerA.driveFrom(peripheralBusCtrl, 0xC0)
+    val timerB = new Timer(gpioCfg.timerConfig)
+    val timerBBridge = timerB.driveFrom(peripheralBusCtrl, 0xD0)
+
+    // Create an UART interface with fixed capabilities
+    val uartCtrlGenerics = UartCtrlGenerics(dataWidthMax = gpioCfg.uartConfig.dataWidthMax,
+      clockDividerWidth = gpioCfg.uartConfig.clockDividerWidth,
+      preSamplingSize = gpioCfg.uartConfig.preSamplingSize,
+      samplingSize = gpioCfg.uartConfig.samplingSize,
+      postSamplingSize = gpioCfg.uartConfig.postSamplingSize)
+    val uartCtrlInitConfig = UartCtrlInitConfig(baudrate = gpioCfg.uartConfig.baudrate,
+      dataLength = gpioCfg.uartConfig.dataLength,
+      parity = gpioCfg.uartConfig.parity,
+      stop = gpioCfg.uartConfig.stop)
+    val uartCtrlMemoryMappedConfig = UartCtrlMemoryMappedConfig(uartCtrlConfig = uartCtrlGenerics,
+      initConfig = uartCtrlInitConfig,
+      busCanWriteClockDividerConfig = false,
+      busCanWriteFrameConfig = false,
+      txFifoDepth = gpioCfg.uartConfig.fifoDepth,
+      rxFifoDepth = gpioCfg.uartConfig.fifoDepth)
+    val uartCtrl = new UartCtrl(uartCtrlGenerics)
+
+    // Map the UART to 0x80 and enable the generation of read interrupts
+    val uartBridge = uartCtrl.driveFrom16(peripheralBusCtrl, uartCtrlMemoryMappedConfig, baseAddress = 0x80)
+    uartBridge.interruptCtrl.readIntEnable := True
+
+    // Tell Spinal that some unneeded signals are allowed to be pruned to avoid warnings
+    uartBridge.interruptCtrl.interrupt.allowPruning()
+    uartBridge.write.streamUnbuffered.ready.allowPruning()
+
+    // Create an interrupt controller, map it to 0xE0 and connect all interrupts
+    val intCtrl = new InterruptCtrl(j1Cfg)
+    val intCtrlBridge = intCtrl.driveFrom(peripheralBusCtrl, 0xE0)
+    intCtrl.io.intsE(intCtrl.io.intsE.high downto j1Cfg.irqConfig.numOfInternalInterrupts) <> io.extInt
+    intCtrl.io.intsE(0) <> uartBridge.interruptCtrl.readInt
+    intCtrl.io.intsE(1) <> timerA.io.interrupt
+    intCtrl.io.intsE(2) <> timerB.io.interrupt
+    cpu.io.intNo <> intCtrl.io.intNo
+    cpu.io.irq <> intCtrl.io.irq
+
+    // Connect the physical UART pins to the outside world
+    io.tx := uartCtrl.io.uart.txd
+    uartCtrl.io.uart.rxd := io.rx
+
+  }
 
 }
 
@@ -101,26 +143,32 @@ object J1SoC {
   def main(args : Array[String]) {
 
     // Configuration of CPU-core and GPIO system
-    val j1Cfg = J1Config.forth
+    val j1Cfg = J1Config.debugIO
     val gpioCfg = GPIOConfig.default
 
-    // Generate HDL files
+    // Generate all VHDL files
     SpinalConfig(genVhdlPkg = true,
                  defaultConfigForClockDomains = globalClockConfig,
-                 defaultClockDomainFrequency = FixedFrequency(100 MHz),
+                 defaultClockDomainFrequency = FixedFrequency(80 MHz),
                  targetDirectory="gen/src/vhdl").generateVhdl({
 
                    // Set name for the synchronous reset
                    ClockDomain.current.reset.setName("clr")
+
+                   // A new system instance
                    new J1SoC(j1Cfg, gpioCfg)
 
                  }).printPruned()
+
+    // Generate all Verilog files
     SpinalConfig(defaultConfigForClockDomains = globalClockConfig,
-                 defaultClockDomainFrequency = FixedFrequency(100 MHz),
+                 defaultClockDomainFrequency = FixedFrequency(80 MHz),
                  targetDirectory="gen/src/verilog").generateVerilog({
 
                    // Set name for the synchronous reset
                    ClockDomain.current.reset.setName("clr")
+
+                   // A new system instance
                    new J1SoC(j1Cfg, gpioCfg)
 
                  }).printPruned()
