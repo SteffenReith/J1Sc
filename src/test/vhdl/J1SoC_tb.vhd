57c82b86f82618aa47e3b80927ee949657f57904
Steffen Reith <steffen.reith@hs-rm.de>
2017-01-24 16:32:01 +0100

    Added a component for UUT

--- src/test/vhdl/J1SoC_tb.vhd
+++ src/test/vhdl/J1SoC_tb.vhd
@@ -22,6 +22,17 @@ end J1SoC_tb;
 
 architecture Behavioral of J1SoC_tb is
 
+  component J1SoC is
+  
+    port(reset     : in std_logic;
+         clk100Mhz : in std_logic;
+         extInt    : in std_logic_vector(0 downto 0);
+         leds      : out std_logic_vector(15 downto 0);
+         rx        : in std_logic;
+         tx        : out std_logic);
+  
+  end component;
+
   -- Clock period definition (100Mhz)
   constant clk_period : time := 10 ns;
 
@@ -41,7 +52,7 @@ architecture Behavioral of J1SoC_tb is
 
 begin
 
-  uut : entity work.J1SoC
+  uut : J1SoC
     port map (clk100Mhz => clk100Mhz,
               reset     => reset,
               extInt    => extInt,
