52e1c751531a3710330b6444e4402f3c9f068ec8
Steffen Reith <steffen.reith@hs-rm.de>
2017-10-19 08:34:19 +0200

    Minor refactoring

--- src/main/scala/SSD.scala
+++ src/main/scala/SSD.scala
@@ -31,33 +31,38 @@ class SSD(j1Cfg  : J1Config,
 
   val io = new Bundle {
 
-    val newValue = in Bits(j1Cfg.wordSize bits)
-    val writeEnableValue = in Bits(ssdCfg.numOfDisplays bits)
-
-    val data = out Vec(Bits(j1Cfg.wordSize bits), ssdCfg.numOfDisplays)
+    // Handle the data to be stored for the displays
+    val newData       = in Bits (j1Cfg.wordSize bits)
+    val enableNewData = in Bits (ssdCfg.numOfDisplays bits)
+    val data          = out Vec(Bits(j1Cfg.wordSize bits), ssdCfg.numOfDisplays)
 
     // The physical signals for the segments and the dot
     val segments = out (Seg7())
-    val dot = out Bool
+    val dot      = out Bool
 
     // Selector for multiplexing
     val selector = out Bits(ssdCfg.numOfDisplays bits)
 
   }.setName("")
 
-  // A look-up table to convert an UInt to the corresponding segments
-  final val convSegTab = Vec(B"0111111", B"0000110", B"1011011", B"1001111",
-                             B"1100110", B"1101101", B"1111101", B"0000111",
-                             B"1111111", B"1101111", B"1110111", B"1111100",
-                             B"0111001", B"1011110", B"1111001", B"1110001")
-
   // Convert a nibble to the segments
-  def convToSegs(nibble : Bits) : Bits = { convSegTab.read(nibble.asUInt) }
+  def convToSegmentVector(nibble : Bits) : Bits = {
+
+    // A look-up table to convert an UInt to the corresponding segments
+    val convSegTab = Vec(B"0111111", B"0000110", B"1011011", B"1001111",
+                         B"1100110", B"1101101", B"1111101", B"0000111",
+                         B"1111111", B"1101111", B"1110111", B"1111100",
+                         B"0111001", B"1011110", B"1111001", B"1110001")
+
+    // Use the lookup-table to generate the segments
+    convSegTab.read(nibble.asUInt)
+
+  }
 
   // Write some infos about the configuration
   println("[J1Sc] SSD-frequency is " + ssdCfg.mplxFrequency.toBigDecimal + "Hz")
-  println("[J1Sc]   Invert segments: " + ssdCfg.invertSegments)
-  println("[J1Sc]   Invert selector: " + ssdCfg.invertSelector)
+  println("[J1Sc]   Segments low-active: " + ssdCfg.invertSegments)
+  println("[J1Sc]   Selector low-active: " + ssdCfg.invertSelector)
 
   // Create a slowed down clock domain for multiplexing the displays
   val ssdClockDomain = ClockDomain.current.newSlowedClockDomain(ssdCfg.mplxFrequency * ssdCfg.numOfDisplays)
@@ -74,9 +79,9 @@ class SSD(j1Cfg  : J1Config,
   // Create all registers holding the nibbles to be display (including the dot)
   val data = Vec(for(i <- 0 to ssdCfg.numOfDisplays - 1) yield {
 
-    // Create the ith register
-    RegNextWhen(io.newValue.msb ## io.newValue(nibbleWidth - 1 downto 0),
-                io.writeEnableValue(i),
+    // Create the ith register and pack the provided data
+    RegNextWhen(io.newData.msb ## io.newData(nibbleWidth - 1 downto 0),
+                io.enableNewData(i),
                 B((nibbleWidth + 1) bits, default -> false))
 
   })
@@ -87,7 +92,7 @@ class SSD(j1Cfg  : J1Config,
 
   // Copy the segments, dots and the selector to the output signals and make them low - active (if configured)
   val selData = data(OHToUInt(ssdArea.selector))
-  val selSegs = convToSegs(selData(selData.high - 1 downto 0))
+  val selSegs = convToSegmentVector(selData(selData.high - 1 downto 0))
   io.segments.assignFromBits(if (ssdCfg.invertSegments) ~selSegs else selSegs)
   io.dot := (if (ssdCfg.invertSegments) ~selData.msb else selData.msb)
   io.selector := (if (ssdCfg.invertSelector) ~ssdArea.selector else ssdArea.selector)
@@ -96,7 +101,7 @@ class SSD(j1Cfg  : J1Config,
   def driveFrom(busCtrl : BusSlaveFactory, baseAddress : BigInt) = new Area {
 
     // The value to be used as a new compare value is constantly driven by the bus
-    busCtrl.nonStopWrite(io.newValue, 0)
+    busCtrl.nonStopWrite(io.newData, 0)
 
     // Make the compare register R/W
     for (i <- 0 to ssdCfg.numOfDisplays - 1) {
@@ -105,7 +110,7 @@ class SSD(j1Cfg  : J1Config,
       busCtrl.read(io.data(i), baseAddress + i, 0)
 
       // Generate the write enable signal for the ith interrupt vector
-      io.writeEnableValue(i) := busCtrl.isWriting(baseAddress + i)
+      io.enableNewData(i) := busCtrl.isWriting(baseAddress + i)
 
     }
 
