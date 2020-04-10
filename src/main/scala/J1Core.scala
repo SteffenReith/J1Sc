8b2182e22aceaf6b41222a52f4988461fde7fb6b
Steffen Reith <streit@streit.cc>
2020-04-11 01:36:02 +0200

    Minor refactoring

--- src/main/scala/J1Core.scala
+++ src/main/scala/J1Core.scala
@@ -38,35 +38,34 @@ class J1Core(cfg : J1Config) extends Component {
 
   }.setName("")
 
-  // Synchronous reset signal
-  val clrActive = ClockDomain.current.isResetActive
-
   // The stall signal is allowed to be pruned (if we have no JTAG)
   internal.stall.allowPruning()
+
+  // Synchronous reset signal
+  val clrActive = ClockDomain.current.isResetActive
 
   // Program counter (note that the MSB is used to control dstack and rstack, hence make is one bit larger)
   val pcN       = UInt(cfg.adrWidth + 1 bits)
   val pc        = RegNextWhen(pcN, !clrActive) init(cfg.startAddress)
   val pcPlusOne = pc + 1
 
-  // Instruction to be executed (inject a call-instruction for handling an interrupt and handle a stall)
-  val stateSelect = internal.stall ## internal.irq
+  // Check for interrupt mode, because afterwards the current instruction has to be executed
+  val returnPC = Mux(internal.irq, pc.asBits, pcPlusOne.asBits)
 
-  // Check status and inject nop (stall mode) or jump (interrupt) when needed
-  val instr = stateSelect.mux(0 -> internal.memInstr,                                   // Normal mode
-                              1 -> B"b010" ## internal.intVec.resize(cfg.wordSize - 3), // Interrupt mode
-                              2 -> J1Config.instrNOP(cfg.wordSize),                     // Stall mode
-                              3 -> J1Config.instrNOP(cfg.wordSize))                     // Stall overrides interrupt
+  // Check status and inject nop (stall mode) or call-instruction (interrupt mode) when needed
+  val stateSelect = internal.stall ## internal.irq
+  val instr = stateSelect.mux(B"00" -> internal.memInstr,                                   // Normal mode
+                              B"01" -> B"b010" ## internal.intVec.resize(cfg.wordSize - 3), // Interrupt mode
+                              B"10" -> J1Config.instrNOP(cfg.wordSize),                     // Stall mode
+                              B"11" -> J1Config.instrNOP(cfg.wordSize))                     // Stall overrides interrupt
 
   // Data stack pointer (set to first entry, which can be arbitrary)
   val dStackPtrN = UInt(cfg.dataStackIdxWidth bits)
   val dStackPtr  = RegNextWhen(dStackPtrN, !internal.stall) init(0)
 
-  // Write enable signal for data stack
-  val dStackWrite = Bool
-
-  // Write enable for return stack
-  val rStackWrite = Bool
+  // Write enable signals for data and return stack
+  val dStackWriteEnable = Bool
+  val rStackWriteEnable = Bool
 
   // Top of stack and next value
   val dtosN = Bits(cfg.wordSize bits)
@@ -76,14 +75,11 @@ class J1Core(cfg : J1Config) extends Component {
   val dStack = Mem(Bits(cfg.wordSize bits), wordCount = (1 << cfg.dataStackIdxWidth))
   dStack.write(address = dStackPtrN,
                data    = dtos,
-               enable  = dStackWrite & !internal.stall)
+               enable  = dStackWriteEnable & !internal.stall)
   val dnos = dStack.readAsync(address = dStackPtr, readUnderWrite = writeFirst)
 
-  // Check for interrupt mode, because afterwards the current instruction has to be executed
-  val retPC = Mux(internal.irq, pc.asBits, pcPlusOne.asBits)
-
   // Set next value for RTOS (check call / interrupt or T -> R ALU instruction)
-  val rtosN = Mux(!instr(instr.high - 3 + 1), (retPC ## B"b0").resized, dtos)
+  val rtosN = Mux(!instr(instr.high - 3 + 1), (returnPC ## B"b0").resized, dtos)
 
   // Return stack pointer, set to first entry (can be arbitrary) s.t. the first write takes place at index 0
   val rStackPtrN = UInt(cfg.returnStackIdxWidth bits)
@@ -93,7 +89,7 @@ class J1Core(cfg : J1Config) extends Component {
   val rStack = Mem(Bits(cfg.wordSize bits), wordCount = (1 << cfg.returnStackIdxWidth))
   rStack.write(address = rStackPtrN,
                data    = rtosN,
-               enable  = rStackWrite & !internal.stall)
+               enable  = rStackWriteEnable & !internal.stall)
   val rtos = rStack.readAsync(address = rStackPtr, readUnderWrite = writeFirst)
 
   // Create an ALU (the AluOp is taken out of the instruction)
@@ -144,16 +140,16 @@ class J1Core(cfg : J1Config) extends Component {
   switch(pc.msb ## instr(instr.high downto (instr.high - 3) + 1)) {
 
     // For a high call push the instruction (== memory access) and for a literal push the value to the data stack
-    is(M"1_---", M"0_1--") {dStackWrite := True; dStackPtrInc := 1}
+    is(M"1_---", M"0_1--") {dStackWriteEnable := True; dStackPtrInc := 1}
 
     // Conditional jump (pop DTOS from data stack)
-    is(M"0_001") {dStackWrite := False; dStackPtrInc := -1}
+    is(M"0_001") {dStackWriteEnable := False; dStackPtrInc := -1}
 
     // ALU instruction (check for a possible push of data, ISA bug can be fixed by '| (instr(1 downto 0) === B"b01")')
-    is(M"0_011"){dStackWrite := funcTtoN; dStackPtrInc := instr(1 downto 0).asSInt.resized}
+    is(M"0_011"){dStackWriteEnable := funcTtoN; dStackPtrInc := instr(1 downto 0).asSInt.resized}
 
     // Don't change the data stack by default
-    default {dStackWrite := False; dStackPtrInc := 0}
+    default {dStackWriteEnable := False; dStackPtrInc := 0}
 
   }
 
@@ -167,16 +163,16 @@ class J1Core(cfg : J1Config) extends Component {
   switch(pc.msb ## instr(instr.high downto (instr.high - 3) + 1)) {
 
     // When we do a high call (the msb of the PC is set) do a pop of return address
-    is(M"1_---") {rStackWrite := False; rStackPtrInc := -1}
+    is(M"1_---") {rStackWriteEnable := False; rStackPtrInc := -1}
 
     // Call instruction or interrupt (push return address to stack)
-    is(M"0_010") {rStackWrite := True; rStackPtrInc := 1}
+    is(M"0_010") {rStackWriteEnable := True; rStackPtrInc := 1}
 
     // Conditional jump (maybe we have to push)
-    is(M"0_011") {rStackWrite := funcTtoR; rStackPtrInc := instr(3 downto 2).asSInt.resized}
+    is(M"0_011") {rStackWriteEnable := funcTtoR; rStackPtrInc := instr(3 downto 2).asSInt.resized}
 
     // Don't change the return stack by default
-    default {rStackWrite := False; rStackPtrInc := 0}
+    default {rStackWriteEnable := False; rStackPtrInc := 0}
 
   }
 
