ec2d0f5a004049d64f71dc6502a0e6ac52c0ba77
Steffen Reith <streit@streit.cc>
2020-06-22 21:30:53 +0200

    Renamed some variables

--- src/main/scala/J1DStack.scala
+++ src/main/scala/J1DStack.scala
@@ -11,56 +11,56 @@ import spinal.core._
 case class J1DStack(cfg : J1Config) {
 
   // The write enable signal for the stack
-  val dStackWriteEnable = Bool
+  val stackWriteEnable = Bool
 
-  // Data stack pointer and next signal
-  val dStackPtrN = UInt(cfg.dataStackIdxWidth bits)
-  val dStackPtr  = Reg(UInt(cfg.dataStackIdxWidth bits)) init (0)
+  // Stack pointer and next signal
+  val stackPtrN = UInt(cfg.dataStackIdxWidth bits)
+  val stackPtr  = Reg(UInt(cfg.dataStackIdxWidth bits)) init (0)
 
   def apply(stall : Bool, dtosN : Bits) : (Bits, Bits, UInt) = {
 
-    // Change the dstack pointer only when the CPU is not stalled
-    when(!stall) { dStackPtr := dStackPtrN }
+    // Change the stack pointer only when the CPU is not stalled
+    when(!stall) { stackPtr := stackPtrN }
 
-    // Top of data stack and next value
+    // Top of stack and next value
     val dtos  = RegNext(dtosN) init(0)
 
     // Data stack with read and write port
     val stackMem = Mem(Bits(cfg.wordSize bits), wordCount = (1 << cfg.dataStackIdxWidth))
-    stackMem.write(address = dStackPtrN,
+    stackMem.write(address = stackPtrN,
                    data    = dtos,
-                   enable  = dStackWriteEnable & !stall)
-    val dnos = stackMem.readAsync(address = dStackPtr, readUnderWrite = writeFirst)
+                   enable  = stackWriteEnable & !stall)
+    val dnos = stackMem.readAsync(address = stackPtr, readUnderWrite = writeFirst)
 
     // Return top and next of stack as a pair and the dstack pointer
-    (dtos, dnos, dStackPtr)
+    (dtos, dnos, stackPtr)
 
   }
 
   def updateDStack(msb : Bool, instr : Bits, funcTtoN : Bool) : Unit = {
 
     // Increment for data stack pointer
-    val dStackPtrInc = SInt(cfg.dataStackIdxWidth bits)
+    val stackPtrInc = SInt(cfg.dataStackIdxWidth bits)
 
     // Handle the update of the data stack
     switch(msb ## instr(instr.high downto (instr.high - 3) + 1)) {
 
       // For a high call push the instruction (== memory access) and for a literal push the value to the data stack
-      is (M"1_---", M"0_1--") {dStackWriteEnable := True; dStackPtrInc := 1}
+      is (M"1_---", M"0_1--") {stackWriteEnable := True; stackPtrInc := 1}
 
       // Conditional jump (pop DTOS from data stack)
-      is (M"0_001") {dStackWriteEnable := False; dStackPtrInc := -1}
+      is (M"0_001") {stackWriteEnable := False; stackPtrInc := -1}
 
       // ALU instruction (check for a possible push of data, ISA bug can be fixed by '| (instr(1 downto 0) === B"b01")')
-      is (M"0_011") {dStackWriteEnable := funcTtoN; dStackPtrInc := instr(1 downto 0).asSInt.resized}
+      is (M"0_011") {stackWriteEnable := funcTtoN; stackPtrInc := instr(1 downto 0).asSInt.resized}
 
       // Don't change the data stack by default
-      default {dStackWriteEnable := False; dStackPtrInc := 0}
+      default {stackWriteEnable := False; stackPtrInc := 0}
 
     }
 
     // Calculate the new value of the data stack pointer
-    dStackPtrN := (dStackPtr.asSInt + dStackPtrInc).asUInt
+    stackPtrN := (stackPtr.asSInt + stackPtrInc).asUInt
 
   }
 
